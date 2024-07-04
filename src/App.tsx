41e3dfe38b4610e7d6f354afd838cee54c765d37
letiendungavsdvdsvdsvs <echnon3@gmail.com>
2024-07-05 00:08:32 +0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Component/Header';
+import Footer from './Component/Footer';
+import Body from './Component/Body';
 function App(){
 return (
 <div>
     <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+
 </div>
 );
 }
---
+++ src/Component/Footer.tsx
@@ -0,0 +1,196 @@
+import React from 'react'
+import '../CSS/Footer.css'
+import '../CSS/main.css'
+
+function Footer(){
+    return (
+        <footer>
+            <div className="top-footer">
+                <div className="container">
+                    <div className="top-footer-content">
+                        <a href="/" className="logo-footer p-15-0" title="Thể thao 247">
+                            <img src="https://thethao247.vn/frontend/images/logo_mobile_footer.png" width="170" alt="Thể thao 247"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-footer">
+                <div className="container">
+                    <div className="menu-footer-content">
+                        <div className="left">
+                            <div className="list_bot_cat">
+                                <p className="title_ver_com ">Tiện ích bóng đá</p>
+                                <ul>
+                                    <li><a href="/lich-thi-dau-bong-da/" title="Lịch thi đấu bóng đá">Lịch thi đấu bóng đá</a></li>
+                                    <li><a href="/ket-qua-bong-da-truc-tuyen/" title="Kết quả bóng đá">Kết quả bóng đá</a></li>
+                                    <li><a href="/nhan-dinh-bong-da-c288/" title="Nhận định bóng đá">Nhận định bóng đá</a></li>
+                                    <li><a href="/livescores/" title="Tỷ số bóng đá">Tỷ số bóng đá</a></li>
+                                    <li><a href="/truc-tiep-bong-da-vip/" title="Trực tiếp bóng đá">Trực tiếp bóng đá</a></li>
+                                    <li><a href="/tin-chuyen-nhuong-c14/" title="Chuyển nhượng bóng đá">Chuyển nhượng bóng đá</a></li>
+                                    <li><a href="/video-c6/" title="Video thể thao">Video thể thao</a></li>
+                                </ul>
+                            </div>
+                            <div className="list_bot_cat">
+                                <p className="title_ver_com ">Giải đấu nổi bật</p>
+                                <ul>
+                                    <li><a href="/bong-da-anh-c8/" title="Ngoại hạng Anh">Ngoại hạng Anh</a></li>
+                                    <li><a href="/champions-league-c13/" title="Cúp C1">Cúp C1</a></li>
+                                    <li><a href="/bong-da-tbn-c9/" title="La Liga">La Liga</a></li>
+                                    <li><a href="/bong-da-y-c10/" title="Serie A">Serie A</a></li>
+                                    <li><a href="/bong-da-duc-c11/" title="Bundesliga">Bundesliga</a></li>
+                                    <li><a href="/v-league-c15/" title="V League">V League</a></li>
+                                    <li><a href="/world-cup" title="World Cup">World Cup</a></li>
+                                    <li><a href="/euro" title="Euro">Euro</a></li>
+                                    <li><a href="/asian-cup" title="Asian Cup">Asian Cup</a></li>
+                                    <li><a href="/europa-league-c75/" title="Cúp C2">Cúp C2</a></li>
+                                    <li><a href="/sea-games" title="SEA Games">SEA Games</a></li>
+                                    <li><a href="/aff-cup" title="AFF Cup">AFF Cup</a></li>
+                                    <li><a href="/bong-da-viet-nam-c1/" title="Bóng đá Việt Nam">Bóng đá Việt Nam</a></li>
+                                </ul>
+                            </div>
+                            <div className="list_bot_cat">
+                                <p className="title_ver_com ">Bảng xếp hạng bóng đá</p>
+                                <ul>
+                                    <li><a href="/livescores/anh/ngoai-hang-anh/standings/" title="BXH Ngoại hạng Anh">BXH Ngoại hạng Anh</a></li>
+                                    <li><a href="/livescores/tay-ban-nha/la-liga/standings/" title="BXH La Liga">BXH La Liga</a></li>
+                                    <li><a href="/livescores/y/serie-a/standings/" title="BXH Ý">BXH Ý</a></li>
+                                    <li><a href="/livescores/chau-au/champions-league/standings/" title="BXH Cúp C1">BXH Cúp C1</a></li>
+                                    <li><a href="/livescores/chau-au/europa-league/standings/" title="BXH Cúp C2">BXH Cúp C2</a></li>
+                                    <li><a href="/livescores/duc/bundesliga/standings/" title="BXH Đức">BXH Đức</a></li>
+                                    <li><a href="/livescores/phap/ligue-1/standings/" title="BXH Pháp">BXH Pháp</a></li>
+                                    <li><a href="/livescores/viet-nam/vleague-1/" title="BXH V League">BXH V League</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <div className="list_bot_cat">
+                                <p className="title_ver_com ">Kết quả bóng đá</p>
+                                <ul>
+                                    <li><a href="/livescores/chau-au/champions-league/results/" title="Kết quả Cúp C1">Kết quả Cúp C1</a></li>
+                                    <li><a href="/livescores/anh/ngoai-hang-anh/results/" title="Kết quả Ngoại hạng Anh">Kết quả Ngoại hạng Anh</a></li>
+                                    <li><a href="/livescores/y/serie-a/results/" title="Kết quả bóng đá Ý">Kết quả bóng đá Ý</a></li>
+                                    <li><a href="/livescores/tay-ban-nha/la-liga/results/" title="Kết quả bóng đá Tây Ban Nha">Kết quả bóng đá Tây Ban Nha</a></li>
+                                    <li><a href="/livescores/duc/bundesliga/results/" title="Kết quả bóng đá Đức">Kết quả bóng đá Đức</a></li>
+                                    <li><a href="/livescores/phap/ligue-1/results/" title="Kết quả bóng đá Pháp">Kết quả bóng đá Pháp</a></li>
+                                    <li><a href="/livescores/chau-au/europa-league/results/" title="Kết quả Cúp C2">Kết quả Cúp C2</a></li>
+                                </ul>
+                            </div>
+                            <div className="list_bot_cat">
+                                <p className="title_ver_com ">Thể thao tổng hợp</p>
+                                <ul>
+                                    <li><a href="/bong-chuyen-c45/" title="Bóng chuyền">Bóng chuyền</a></li>
+                                    <li><a href="/quan-vot-tennis-c4/" title="Quần vợt">Quần vợt</a></li>
+                                    <li><a href="/esports-c180/" title="Esports">Esports</a></li>
+                                    <li><a href="/xe-co-c191/" title="Xe cộ">Xe cộ</a></li>
+                                    <li><a href="/vo-thuat-c228/" title="Võ">Võ</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="tag-footer">
+                <div className="container">
+                    <div className="pkg">
+                        <a href="/the-thao-24h.html" className="key_footer_tag" target="_blank" rel="noopener" title="Thể Thao 24h">Thể Thao 24h</a>
+                        <a className="key_footer_tag" target="_blank" href="https://www.techz.vn/" rel="noopener" title="Techz.vn">Công nghệ</a>
+                    </div>
+                </div>
+            </div>
+            <div className="info-footer">
+                <div className="container">
+                    <div className="info-footer-content">
+                        <div className="col_app_com">
+                            <p className="title_ver_com ">App Mobile</p>
+                            <ul className="list_ver_com" id="dowload_footer">
+                                <li>
+                                    <a href="https://apps.apple.com/app/id1485962204" target="_blank"
+                                       rel="nofollow noopener" className="icon_appstore">
+                                        <i className="fab fa-apple"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://play.google.com/store/apps/details?id=vn.thethao247.android&hl=vi"
+                                       target="_blank" rel="nofollow noopener" className="icon_appstore icon_android">
+                                        <i className="fab fa-android"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col_ver_com">
+                            <p className="title_ver_com ">Liên hệ</p>
+                            <ul className="list_ver_com">
+                                <li><a href="/gioi-thieu-p1.html">Giới thiệu</a></li>
+                                <li><a href="/chinh-sach-bao-mat-p5.html">Chính sách bảo mật</a></li>
+                                <li><a href="/bao-gia.html" rel="nofollow">Quảng cáo</a></li>
+                            </ul>
+                        </div>
+                        <div className="col_ver_com">
+                            <p className="title_ver_com ">Social</p>
+                            <ul className="list_ver_com">
+                                <li>
+                                    <a href="https://www.youtube.com/channel/UCxgM0479aF4tTYk2aPDn39Q"
+                                       title="Youtube Thể Thao 247" target="_blank" rel="nofollow noopener">
+                                        <i className="icon-youtube"></i> Youtube
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://oa.zalo.me/thethao247" title="Zalo Thể Thao 247" target="_blank"
+                                       rel="nofollow noopener">
+                                        <i className="icon-zalo"></i> Zalo
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/thethao247.vn" title="Facebook Thể Thao 247"
+                                       target="_blank" rel="nofollow noopener">
+                                        <i className="icon-facebook"></i> Facebook
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/thethao247vn" title="Twitter Thể Thao 247" target="_blank"
+                                       rel="nofollow noopener">
+                                        <i className="icon-twitter"></i> Twitter
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/rss.html" rel="nofollow" title="RSS Thể Thao 247">
+                                        <i className="icon-rss"></i> RSS
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.tiktok.com/@thethao247.vn" rel="nofollow noopener" title="Tiktok Thể Thao 247" target="_blank">
+                                        <svg viewBox="0 0 291.72499821636245 291.1" xmlns="https://www.w3.org/2000/svg" width="15" height="15"><path d="M219.51 291.1H71.58C32.05 291.1 0 259.05 0 219.51V71.58C0 32.05 32.05 0 71.58 0h147.93c39.53 0 71.58 32.05 71.58 71.58v147.93c.01 39.54-32.04 71.59-71.58 71.59z"/><g fill="#25f4ee"><path d="M120.96 123.89v-8.8a64.83 64.83 0 0 0-9.23-.79c-29.93-.06-56.42 19.33-65.41 47.87s1.62 59.62 26.18 76.71c-25.77-27.58-24.3-70.83 3.28-96.6a68.425 68.425 0 0 1 45.18-18.39z"/><path d="M122.62 223.53c16.73-.02 30.48-13.2 31.22-29.92V44.44h27.25a50.7 50.7 0 0 1-.79-9.44h-37.27v149.02c-.62 16.8-14.41 30.11-31.22 30.14-5.02-.04-9.97-1.28-14.42-3.6a31.276 31.276 0 0 0 25.23 12.97zM231.98 95.05v-8.29c-10.03 0-19.84-2.96-28.19-8.51a51.63 51.63 0 0 0 28.19 16.8z"/></g><path d="M203.8 78.26a51.301 51.301 0 0 1-12.76-33.89h-9.95a51.564 51.564 0 0 0 22.71 33.89zM111.73 151.58c-17.28.09-31.22 14.17-31.13 31.45a31.293 31.293 0 0 0 16.71 27.53c-10.11-13.96-6.99-33.48 6.97-43.6a31.191 31.191 0 0 1 18.34-5.93c3.13.04 6.24.53 9.23 1.45v-37.93c-3.05-.46-6.14-.7-9.23-.72h-1.66v28.84c-3.01-.82-6.12-1.18-9.23-1.09z" fill="#fe2c55"/><path d="M231.98 95.05v28.84a88.442 88.442 0 0 1-51.69-16.8v75.77c-.08 37.81-30.75 68.42-68.56 68.42a67.816 67.816 0 0 1-39.22-12.4c25.73 27.67 69.02 29.25 96.7 3.52a68.397 68.397 0 0 0 21.83-50.09v-75.56a88.646 88.646 0 0 0 51.76 16.58V96.21c-3.64-.02-7.26-.4-10.82-1.16z" fill="#fe2c55"/><path d="M180.29 182.87V107.1a88.505 88.505 0 0 0 51.76 16.58V94.84a51.73 51.73 0 0 1-28.26-16.58 51.634 51.634 0 0 1-22.71-33.89h-27.25v149.24c-.71 17.27-15.27 30.69-32.54 29.99a31.278 31.278 0 0 1-24.06-12.9c-15.29-8.05-21.16-26.97-13.11-42.26a31.274 31.274 0 0 1 27.53-16.71c3.13.03 6.24.51 9.23 1.44V123.9c-37.74.64-67.82 32.19-67.18 69.93a68.353 68.353 0 0 0 18.73 45.86 67.834 67.834 0 0 0 39.29 11.61c37.82-.01 68.49-30.62 68.57-68.43z" fill="#fff"/></svg>
+                                        Tiktok
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col_info_com">
+                            <p className="title_ver_com "><strong>CÔNG TY CP DỊCH VỤ TRUYỀN THÔNG THỂ THAO 247</strong></p>
+                            <p>Người chịu trách nhiệm quản lý nội dung: <strong>Nguyễn Huy Hoàn</strong></p>
+                            <p>Giấy phép trang tin điện tử tổng hợp số: <strong>1749/GP-TTĐT</strong> do Sở Thông tin và Truyền
+                                thông Hà Nội cấp ngày 28/04/2017
+                            </p>
+                            <p><strong>Địa chỉ giao dịch:</strong> Căn 12, Mộc Lan 6 (ML6-12), khu đô thị Vinhomes Green Bay,
+                                phường Mễ Trì, quận Nam Từ Liêm, Hà Nội</p>
+                            <p><strong>Hotline:</strong> <a href="tel:0847100247">0847 100 247</a> / <strong>Email:</strong>
+                                <a href="mailto:contact@thethao247.vn">contact@thethao247.vn</a></p>
+                            <p><strong>Truyền thông và Quảng cáo:</strong> <a href="tel:0985233950">0985 233 950</a></p>
+                            <p style={{color:"darkgray"}}><strong>Trụ sở chính:</strong> Tầng 4, Tòa tháp Ngôi Sao - Star Tower,
+                                đường Dương Đình Nghệ - Phường Yên Hòa - Quận Cầu Giấy - Hà Nội</p>
+                            <a href="https://www.dmca.com/Protection/Status.aspx?ID=d31a2a19-f919-485f-93b2-8697b3a6ff75&amp;refurl=https://thethao247.vn/"
+                               target="_blank" rel="nofollow noopener" title="DMCA.com Protection Status" className="dmca-badge">
+                                <img
+                                style={{marginLeft: -5}}
+                                src="https://images.dmca.com/Badges/dmca-badge-w250-5x1-06.png?ID=d31a2a19-f919-485f-93b2-8697b3a6ff75"
+                                alt="DMCA.com Protection Status" /></a>
+                            <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
