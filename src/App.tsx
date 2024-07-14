716cfeb314c6f4852e9af61b59ab91085652ab66
letiendungavsdvdsvdsvs <echnon3@gmail.com>
2024-07-15 06:52:42 +0700

    sort and load more

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './Component/Header';
 import Footer from './Component/Footer';
-import Body from './Component/Body';
-function App(){
-return (
-<div>
-    <Header></Header>
-    <Body></Body>
-    <Footer></Footer>
+// import CategoryArticleBody from "./Component/CategoryArticleBody";
+import RSSFeed from './Component/RSSFeed';
 
-</div>
-);
-}
+const App: React.FC = () => {
+    const rssUrl = 'https://thethao247.vn/trang-chu.rss';
+    return (
+        <>
+            <Header/>
+            <RSSFeed />
+            <Footer/>
+        </>
+    );
+};
+
 export default App;
--- src/Component/Body.tsx
+++ src/Component/Body.tsx
@@ -4,7 +4,102 @@ import '../CSS/main.css'
 
 function Body(){
     return (
-        <div className="body"></div>
+        <div className="w360 box_latest">
+            <div className="caption">
+                <h2>
+                    <a className="title" href="#" title="Tin mới nhất">
+                        <span>Tin mới nhất</span>
+                    </a>
+                </h2>
+            </div>
+            <div className="content">
+                <ul>
+
+                    <li>
+                        <a href="https://thethao247.vn/460-sao-tre-bong-chuyen-nu-viet-nam-lap-ky-luc-tai-giai-chau-a-d334647.html"
+                           className="thumb" title="Sao trẻ bóng chuyền nữ Việt Nam lập kỷ lục tại giải Châu Á"><img
+                            className=""
+                            src="https://cdn-img.thethao247.vn/resize_496x356/storage/files/btvttth1/2024/07/06/thumbnail-2024-07-06t213410541-1720276462-213555avatar.png"
+                            width="248" height="178" alt="Sao trẻ bóng chuyền nữ Việt Nam lập kỷ lục tại giải Châu Á"/></a>
+                        <div className="text">
+                            <h3><a
+                                href="https://thethao247.vn/460-sao-tre-bong-chuyen-nu-viet-nam-lap-ky-luc-tai-giai-chau-a-d334647.html"
+                                className="title" title="Sao trẻ bóng chuyền nữ Việt Nam lập kỷ lục tại giải Châu Á">Sao
+                                trẻ bóng chuyền nữ Việt Nam lập kỷ lục tại giải Châu Á</a>
+                            </h3>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a href="https://thethao247.vn/352-can-trong-truoc-chieu-tro-lua-dao-moi-treo-ma-qr-tang-tien-de-du-cai-ma-doc-d334644.html"
+                           className="thumb"
+                           title="Cẩn trọng trước chiều trò lừa đảo mới: Treo 'mã QR tặng tiền' để dụ cài mã độc"><img
+                            className=""
+                            src="https://cdn-img.thethao247.vn/resize_496x356/storage/files/quanganh/2024/07/06/static-imagesvnncdnnet-vps-images-publish-000001-000003-2024-7-6-canh-bao-thu-doan-lua-dao-rat-moi-quet-ma-qr-code-de-nhan-tien-100-1720273366-204406avatar.png"
+                            width="248" height="178"
+                            alt="Cẩn trọng trước chiều trò lừa đảo mới: Treo 'mã QR tặng tiền' để dụ cài mã độc"/></a>
+                        <div className="text">
+                            <h3><a
+                                href="https://thethao247.vn/352-can-trong-truoc-chieu-tro-lua-dao-moi-treo-ma-qr-tang-tien-de-du-cai-ma-doc-d334644.html"
+                                className="title"
+                                title="Cẩn trọng trước chiều trò lừa đảo mới: Treo 'mã QR tặng tiền' để dụ cài mã độc">Cẩn
+                                trọng trước chiều trò lừa đảo mới: Treo "mã QR tặng tiền" để dụ cài mã độc</a>
+                            </h3>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a href="https://thethao247.vn/400-lich-thi-dau-lmht-tai-esports-world-cup-2024-d325665.html"
+                           className="thumb" title="Lịch thi đấu LOL Esports World Cup 2024 mới nhất hôm nay"><img
+                            className=""
+                            src="https://cdn-img.thethao247.vn/resize_496x356/storage/files/haibui/2024/07/05/6687c51691326.png"
+                            width="248" height="178"
+                            alt="Lịch thi đấu LOL Esports World Cup 2024 mới nhất hôm nay"/></a>
+                        <div className="text">
+                            <h3><a
+                                href="https://thethao247.vn/400-lich-thi-dau-lmht-tai-esports-world-cup-2024-d325665.html"
+                                className="title" title="Lịch thi đấu LOL Esports World Cup 2024 mới nhất hôm nay">Lịch
+                                thi đấu LOL Esports World Cup 2024 mới nhất hôm nay</a>
+                            </h3>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a href="https://thethao247.vn/460-bong-chuyen-nu-viet-nam-huong-toi-thu-hang-cao-tai-fivb-challenger-cup-2024-d334638.html"
+                           className="thumb"
+                           title="Bóng chuyền nữ Việt Nam hướng tới thứ hạng cao tại FIVB Challenger Cup 2024"><img
+                            className=""
+                            src="https://cdn-img.thethao247.vn/resize_496x356/storage/files/btvttth1/2024/07/06/thumbnail-2024-07-06t194932248-1720270182-195156avatar.png"
+                            width="248" height="178"
+                            alt="Bóng chuyền nữ Việt Nam hướng tới thứ hạng cao tại FIVB Challenger Cup 2024"/></a>
+                        <div className="text">
+                            <h3><a
+                                href="https://thethao247.vn/460-bong-chuyen-nu-viet-nam-huong-toi-thu-hang-cao-tai-fivb-challenger-cup-2024-d334638.html"
+                                className="title"
+                                title="Bóng chuyền nữ Việt Nam hướng tới thứ hạng cao tại FIVB Challenger Cup 2024">Bóng
+                                chuyền nữ Việt Nam hướng tới thứ hạng cao tại FIVB Challenger Cup 2024</a>
+                            </h3>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a href="https://thethao247.vn/460-ket-qua-bong-chuyen-nu-viet-nam-vs-ch-sec-17h30-ngay-6-7-d334617.html"
+                           className="thumb" title="Bóng chuyền nữ Việt Nam dừng bước tại bán kết giải thế giới"><img
+                            className=""
+                            src="https://cdn-img.thethao247.vn/resize_496x356/storage/files/btvttth1/2024/07/06/thumbnail-2024-07-06t130810302-1720246100-130918avatar.png"
+                            width="248" height="178" alt="Bóng chuyền nữ Việt Nam dừng bước tại bán kết giải thế giới"/></a>
+                        <div className="text">
+                            <h3><a
+                                href="https://thethao247.vn/460-ket-qua-bong-chuyen-nu-viet-nam-vs-ch-sec-17h30-ngay-6-7-d334617.html"
+                                className="title" title="Bóng chuyền nữ Việt Nam dừng bước tại bán kết giải thế giới">Bóng
+                                chuyền nữ Việt Nam dừng bước tại bán kết giải thế giới</a>
+                            </h3>                    
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     )
 }
 
