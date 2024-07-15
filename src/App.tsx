f91568180fec53e073d644a3c8c34784c469e348
kobitcode77 <21130382@st.hcmuaf.edu.vn>
2024-07-15 22:02:27 +0700

    change url when click category on header

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,25 @@ import './App.css';
 import Header from './Component/Header';
 import Footer from './Component/Footer';
 import CategoryArticleBody from "./Component/CategoryArticleBody";
-import RSSFeed from './Component/RSSFeed';
 import HomeBody from "./Component/HomeBody";
 
 const App: React.FC = () => {
-                              const [currentComponent, setCurrentComponent] = useState<'home' | 'category'>('home');
-                              const [rssUrl, setRssUrl] = useState("https://thethao247.vn/bong-da-viet-nam-c1.rss")
-                          const handleCategoryChange = (newRssUrl: string) => {
-                          setRssUrl(newRssUrl);
-                          setCurrentComponent('category');
-                          }
-                              const handleHomeClick = () => {
-                          setCurrentComponent('home');
-                          };
-                              return (
-                              <div>
-                              <Header onCategoryChange={handleCategoryChange} onHomeClick={handleHomeClick}/>
-                          {currentComponent === 'home' ? <HomeBody /> : <CategoryArticleBody rssUrl={rssUrl} />}
-                          <Footer/>
-                          </div>
-                          );
-                          };
+    const [currentComponent, setCurrentComponent] = useState<'home' | 'category'>('home');
+    const [rssUrl, setRssUrl] = useState("https://thethao247.vn/bong-da-viet-nam-c1.rss")
+    const handleCategoryChange = (newRssUrl: string) => {
+        setRssUrl(newRssUrl);
+        setCurrentComponent('category');
+    }
+    const handleHomeClick = () => {
+        setCurrentComponent('home');
+    };
+    return (
+        <div>
+            <Header onCategoryChange={handleCategoryChange} onHomeClick={handleHomeClick}/>
+            {currentComponent === 'home' ? <HomeBody/> : <CategoryArticleBody key={rssUrl} rssUrl={rssUrl}/>}
+            <Footer/>
+        </div>
+    );
+};
 
 export default App;
