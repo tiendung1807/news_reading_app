b25811f26ba4115d3c0f3239fab6cc38fb9049a9
letiendungavsdvdsvdsvs <echnon3@gmail.com>
2024-07-15 07:30:49 +0700

    sort, load more

---
+++ src/Component/SortComponent.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../CSS/SortComponent.css';
+
+interface SortComponentProps {
+    setSortKey: (key: string) => void;
+    setSortOrder: (order: string) => void;
+}
+
+const SortComponent: React.FC<SortComponentProps> = ({ setSortKey, setSortOrder }) => {
+    const handleSortKeyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortKey(e.target.value);
+    };
+
+    const handleSortOrderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortOrder(e.target.value);
+    };
+
+    return (
+        <div className="sort-container">
+            <label htmlFor="sortKey">Sắp xếp theo:</label>
+            <select id="sortKey" onChange={handleSortKeyChange}>
+                <option value="pubDate">Ngày</option>
+                <option value="title">Tiêu đề</option>
+            </select>
+            <label htmlFor="sortOrder">Thứ tự:</label>
+            <select id="sortOrder" onChange={handleSortOrderChange}>
+
+                <option value="asc">Cũ nhất (Tăng dần)</option>
+                <option value="desc">Mới nhất (Giảm dần)</option>
+            </select>
+        </div>
+    );
+};
+
+export default SortComponent;
